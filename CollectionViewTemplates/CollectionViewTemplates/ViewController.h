551d6002ef1fd9745814a0c75f50bb8f621ac497
Gaurav Pal <gaurav.pal@hightail.com>
2016-03-13 19:14:13 -0700

    Create pods project workspace for collectionview templates

---
+++ CollectionViewTemplates/CollectionViewTemplates/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CollectionViewTemplates
+//
+//  Created by Gaurav Pal on 3/13/16.
+//  Copyright © 2016 help. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CollectionViewTemplates/CollectionViewTemplatesTests/CollectionViewTemplatesTests.m
@@ -0,0 +1,39 @@
+//
+//  CollectionViewTemplatesTests.m
+//  CollectionViewTemplatesTests
+//
+//  Created by Gaurav Pal on 3/13/16.
+//  Copyright © 2016 help. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CollectionViewTemplatesTests : XCTestCase
+
+@end
+
+@implementation CollectionViewTemplatesTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
